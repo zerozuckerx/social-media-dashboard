4d85ac2d9a64fbcc9ea5e6df40c9f8eb1ab3a633
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2023-04-22 20:35:41 +0200

    done I guess, phew! could make changes to find common classes for top and bottom, they share the same bg and textcolor styling

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const toggleButton = document.querySelector("input[type=checkbox]");
 const body = document.querySelector("body");
-const topCards = document.querySelector(".top-cards");
 
 toggleButton.addEventListener("change", () => darkModeOn());
 
 function darkModeOn() {
   body.classList.toggle("dark-mode");
-  // topCards.classList.toggle("dark-mode");
 };
