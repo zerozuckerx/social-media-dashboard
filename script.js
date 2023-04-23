60fff45f1dd77c353bbae44c6182da8a6f17591d
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2023-04-23 22:58:51 +0200

    tried setting light/dark mode with localStorage, doestn't work yet, problem with using localStorage value as variable

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+localStorage.setItem("mode", "light-mode")
+let mode = localStorage.getItem("mode"); //apparently not working
+console.log(mode);
+
 const toggleButton = document.querySelector("input[type=checkbox]");
 const body = document.querySelector("body");
 
 toggleButton.addEventListener("change", () => darkModeOn());
 
 function darkModeOn() {
+  if (localStorage.getItem("mode") === "light-mode") {
+    localStorage.setItem("mode", "dark-mode")
+    console.log("set to dark");
+  } else {
+    localStorage.setItem("mode", "light-mode")
+    console.log("set to light");
+  }
   body.classList.toggle("dark-mode");
 };
